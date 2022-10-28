aec01791ad49faeb547ca3dee35b2bc85963553a
Shohruh Tukhtashev <87192481+Shohruhtukhtashev@users.noreply.github.com>
2022-10-28 20:11:30 +0500

    While

---
+++ 6-dars.py
@@ -0,0 +1,79 @@
+'''n=int(input('n='))
+i=2
+while i<=n:
+	print(i)
+	i=i+2
+'''
+
+'''
+n=int(input('n='))
+i=1 s=0
+while i<=n:
+	s+=i
+	i+=1
+
+print(s)
+'''
+'''
+n=int(input('n='))
+i=1
+k=1
+while i<=n:
+	k*=i
+	i+=1
+print(k)
+'''
+'''k=1
+while k<=9:
+	k+=1
+	print(k)
+'''
+'''k=1
+s=0
+while k<=5:
+	s+=k
+	k+=1
+print(s)
+'''
+'''n=int(input('n='))
+s=0
+k=1
+while k<=n:
+	s+=k
+	k+=1
+print(f"{n} gacha bo'lgan raqamlar yig'indisi {s} ga teng.")
+'''
+'''n=int(input('n='))
+k=1
+while k<=n-1:
+	print(k)
+	k+=2
+'''
+#Faktorial
+'''n=int(input('n='))
+k=1
+s=1
+while k<=n:
+	s*=k
+	k+=1
+print(s)
+'''
+#O'rta geometrik
+'''
+n=int(input('n='))
+k=1
+s=1
+while k<=n:
+	s*=k
+	k+=1
+print(s**(1/5))
+'''
+# n sonidan boshlab orqaga sanagich
+'''
+n=int(input('n='))
+k=0
+while k!=n:
+	print(n)
+	n-=1
+'''
+
